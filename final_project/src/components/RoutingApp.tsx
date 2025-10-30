c4e3a404dcd48fee667e8830523cf2b30d5195a5
Sammy389180 <panuwatjintaraksa@gmail.com>
2025-10-31 03:02:30 +0700

    commit

--- final_project/src/components/RoutingApp.tsx
+++ final_project/src/components/RoutingApp.tsx
@@ -1,5 +1,5 @@
 // 1) Import
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
@@ -8,6 +8,7 @@ import ApartmentList from './ApartmentList';
 // Level 1 : RoutingApp (npm install react-router-dom @types/react - router - dom) 
 // 2) Create RoutingApp component
 const RoutingApp: React.FC = () => {
+  const [apartmentsMenuOpen, setApartmentsMenuOpen] = useState(false);
   // 2.2 JSX with routing structure 
   return (
     <BrowserRouter>
@@ -19,10 +20,36 @@ const RoutingApp: React.FC = () => {
             {/* 2.3.4) Link */}
             <li><Link to="/" className="text-white hover:underline">Home</Link></li>
             <li><Link to="/about" className="text-white hover:underline">About</Link></li>
-            <li><Link to="/apartments/list" className="text-white hover:underline">List/Edit/Delete
-              Apartment</Link></li>
-            <li><Link to="/apartments/new" className="text-white hover:underline">Add New
-              Apartment</Link></li>
+            <li className="relative">
+              <button
+                type="button"
+                onClick={() => setApartmentsMenuOpen(!apartmentsMenuOpen)}
+                className="text-white hover:underline flex items-center"
+                aria-expanded={apartmentsMenuOpen}
+              >
+                Apartments
+                <svg className="ml-2 w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path></svg>
+              </button>
+
+              {apartmentsMenuOpen && (
+                <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg z-20">
+                  <Link
+                    to="/apartments/list"
+                    onClick={() => setApartmentsMenuOpen(false)}
+                    className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
+                  >
+                    List / Edit / Delete
+                  </Link>
+                  <Link
+                    to="/apartments/new"
+                    onClick={() => setApartmentsMenuOpen(false)}
+                    className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
+                  >
+                    Add New Apartment
+                  </Link>
+                </div>
+              )}
+            </li>
           </ul>
         </nav>
         {/* Routes and Route Handling */}
