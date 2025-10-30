822b49cbf384b5ba635f6a8c21936ea5088efff5
Sammy389180 <panuwatjintaraksa@gmail.com>
2025-10-31 00:50:34 +0700

    commite

--- final_project/src/components/RoutingApp.tsx
+++ final_project/src/components/RoutingApp.tsx
@@ -1,26 +1,15 @@
-<<<<<<< HEAD
 // 1) Import
 import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Home from './Home';
 import ApartmentList from './ApartmentList';
-=======
-// src/components/RoutingApp.tsx
-import { Routes, Route } from "react-router-dom";
-import Home from "./Home";
-import Profile from "./Profile";
-import Contact from "./Contact";
-import Register from "./Register";
-import Post from "./Post";
-import DormDetail from "./DormDetail";
->>>>>>> 5f08484ea72c24f76b9789d38a18a7aa84a96c91
+import ApartmentForm from './ApartmentForm';
 
 // Level 1 : RoutingApp (npm install react-router-dom @types/react - router - dom) 
 // 2) Create RoutingApp component
 const RoutingApp: React.FC = () => {
   // 2.2 JSX with routing structure 
   return (
-<<<<<<< HEAD
     <BrowserRouter>
       {/* 2.2.1) BrowserRouter */}
       <div className="min-h-screen bg-gray-100">
@@ -30,6 +19,7 @@ const RoutingApp: React.FC = () => {
             {/* 2.3.4) Link */}
             <li><Link to="/" className="text-white hover:underline">Home</Link></li>
             <li><Link to="/apartments" className="text-white hover:underline">Apartments</Link></li>
+            <li><Link to="/apartments/new" className="text-white hover:underline">Add Apartment</Link></li>
           </ul>
         </nav>
         {/* Routes and Route Handling */}
@@ -39,20 +29,11 @@ const RoutingApp: React.FC = () => {
             {/* 2.2.3) Route and path */}
             <Route path="/" element={<Home />} />
             <Route path="/apartments" element={<ApartmentList />} />
+            <Route path="/apartments/new" element={<ApartmentForm />} />
           </Routes>
         </div>
       </div>
     </BrowserRouter>
-=======
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/post" element={<Post />} />
-      <Route path="/dorm/:id" element={<DormDetail />} />
-    </Routes>
->>>>>>> 5f08484ea72c24f76b9789d38a18a7aa84a96c91
   );
 };
 
